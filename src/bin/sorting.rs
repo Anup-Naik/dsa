a02b3231ed73f2e7a79dea708a3da22ecaea0bfa
Anup-Naik <anupnaik54810@gmail.com>
2025-11-14 18:41:22 +0530

    Fri Nov 14 06:41:22 PM IST 2025

--- src/bin/sorting.rs
+++ src/bin/sorting.rs
@@ -1,34 +1,85 @@
-
-fn swap(v: &mut Vec<i32>, i: usize, j: usize) {
-    let temp = v[i];
-    v[i] = v[j];
-    v[j] = temp;
-}
-
-fn selection_sort(v: &mut Vec<i32>, n: usize) {
-    for i in 0..n {
+// Select minimum value and insert from beginning
+// Time complexity O(n^2) - Best, Avg, Worst
+fn selection_sort(mut v: Vec<i32>, n: usize) -> Vec<i32> {
+    for i in 0..n - 1 {
         let mut min = i;
-        for j in i..n {
+        for j in i + 1..n {
             if v[j] < v[min] {
                 min = j;
             }
         }
-        swap(v, i, min);
+        v.swap(i, min);
     }
+    v
 }
+
+// larger values bubble up to end
+// Time complexity O(n^2) - Avg, Worst | O(n) - Best
+fn bubble_sort(mut v: Vec<i32>, n: usize) -> Vec<i32> {
+    let mut swapped;
+    for i in (0..=n - 1).rev() {
+        swapped = false;
+        for j in 0..i {
+            if v[j] > v[j + 1] {
+                v.swap(j, j + 1);
+                swapped = true;
+            }
+        }
+        if !swapped {
+            break;
+        }
+    }
+    v
+}
+
+// insert the value in its correct place
+// Time complexity O(n^2) - Avg, Worst | O(n) - Best
+fn insertion_sort(mut v: Vec<i32>, n: usize) -> Vec<i32> {
+    for i in 1..n {
+        let mut j = i;
+        while j > 0 && v[j] < v[j - 1] {
+            v.swap(j, j - 1);
+            j -= 1;
+        }
+    }
+    v
+}
+
+use std::thread::{self, JoinHandle};
+use std::time::Instant;
 fn main() {
     let mut buf = String::new();
     std::io::stdin()
         .read_line(&mut buf)
         .expect("Error Reading n");
+
     let n: usize = buf.trim().parse().expect("Error Parsing Value");
-    let mut v: Vec<i32>;
-    v = std::io::stdin()
+    let v: Vec<i32> = std::io::stdin()
         .lines()
         .map(|v| v.unwrap().parse().unwrap())
         .take(n)
         .collect();
+
     println!("Original Array {:#?}", v);
-    selection_sort(&mut v, n);
-    println!("Sorted Array {:#?}", v);
+    let sorting_algorithms = [selection_sort, bubble_sort, insertion_sort];
+    let mut join_handles: Vec<JoinHandle<_>> = vec![];
+    sorting_algorithms
+        .iter()
+        .enumerate()
+        .for_each(|(i, &sort)| {
+            let v = v.clone();
+            let jh = thread::spawn(move || {
+                let start = Instant::now();
+                let arr = sort(v, n);
+                let tt = start.elapsed();
+                println!(
+                    "Sorting Algorithm {} => {:#?} .Time Taken {:#?}",
+                    i, arr, tt
+                );
+            });
+            join_handles.push(jh);
+        });
+    for j in join_handles {
+        j.join().unwrap();
+    }
 }
